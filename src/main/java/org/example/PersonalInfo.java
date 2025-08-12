f820d4eb5c1067369c3f5537058b9cac02a226be
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-08-12 20:04:32 +0700

    First commit: Demo IN/OUT

---
+++ src/main/java/org/example/PersonalInfo.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String name;
+        int age;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+
+        //output
+        System.out.println("Your name is " + name + "you are is" + age);
+    }
+}
